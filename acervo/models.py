b9470417bcaa0310bf781a17bf7baeadc168fe48
victorelizeu <joaogamer759@gmail.com>
2026-09-16 20:59:31 -0300

    fix: Models corrigido.

--- acervo/models.py
+++ acervo/models.py
@@ -25,16 +25,16 @@ class Tipo_Acervo(models.Model):
 
 class Categoria(models.Model):
     estado = [
-        ("000 – Generalidades e Informação: Obras gerais, enciclopédias, jornais e biblioteconomia"),
-        ("100 – Filosofia e Psicologia: Ética, lógica e investigações sobre a mente humana"),
-        ("200 – Religião e Teologia: Mitologia, teologia e estudos sobre crenças e religiões"),
-        ("300 – Ciências Sociais e Direito: Política, economia, sociologia, educação e leis"),
-        ("400 – Linguística e Idiomas: Gramáticas, dicionários e estudos de línguas"),
-        ("500 – Ciências Puras (Exatas e Naturais): Matemática, física, química, biologia e astronomia"),
-        ("600 – Ciências Aplicadas (Tecnologia): Medicina, engenharia, agricultura e administração"),
-        ("700 – Artes e Recreação: Pintura, música, arquitetura, esportes e lazer"),
-        ("800 – Literatura: Poesia, romances, contos, crônicas e crítica literária"),
-        ("900 – História e Geografia: Biografias, viagens e acontecimentos históricos")
+        ("000 –", "Generalidades e Informação: Obras gerais, enciclopédias, jornais e biblioteconomia"),
+        ("100 –", "Filosofia e Psicologia: Ética, lógica e investigações sobre a mente humana"),
+        ("200 –", "Religião e Teologia: Mitologia, teologia e estudos sobre crenças e religiões"),
+        ("300 – ", "Ciências Sociais e Direito: Política, economia, sociologia, educação e leis"),
+        ("400 – ", "Linguística e Idiomas: Gramáticas, dicionários e estudos de línguas"),
+        ("500 – ", "Ciências Puras (Exatas e Naturais): Matemática, física, química, biologia e astronomia"),
+        ("600 – ", "Ciências Aplicadas (Tecnologia): Medicina, engenharia, agricultura e administração"),
+        ("700 – ", "Artes e Recreação: Pintura, música, arquitetura, esportes e lazer"),
+        ("800 – ", "Literatura: Poesia, romances, contos, crônicas e crítica literária"),
+        ("900 – ", "História e Geografia: Biografias, viagens e acontecimentos históricos")
     ]
 
     categorias = models.CharField(
