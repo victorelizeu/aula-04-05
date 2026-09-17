2957fbcfed4ef837d9f013aa81877b715f558239
victorelizeu <joaogamer759@gmail.com>
2026-09-16 21:57:42 -0300

    feat: urls.py, style.css, lista.html e outros modificados.

--- biblioteca/urls.py
+++ biblioteca/urls.py
@@ -19,5 +19,5 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("admin/", admin.site.urls), path("", "acervo.urls")
+    path("admin/", admin.site.urls), path("", include("acervo.urls"))
 ]
